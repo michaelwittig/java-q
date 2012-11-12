04c7a5d010055de40610ac470599fcf6c9cf00dc
Michael Wittig <michael.wittig@tullius-walden.com>
2012-11-12 16:21:33 +0100

    added functions

---
+++ src/main/java/de/cinovo/q/connector/impl/KXSyncCommandFunction.java
@@ -0,0 +1,51 @@
+// -------------------------------------------------------------------------------
+// Copyright (c) 2011-2012 Cinovo AG
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Apache License, Version 2.0
+// which accompanies this distribution, and is available at
+// http://www.apache.org/licenses/LICENSE-2.0.html
+// -------------------------------------------------------------------------------
+
+package de.cinovo.q.connector.impl;
+
+import java.io.IOException;
+
+import kx.c;
+import kx.c.KException;
+import de.cinovo.q.connector.KXException;
+import de.cinovo.q.query.Function;
+import de.cinovo.q.query.Result;
+
+/**
+ * KX string command with result.
+ * 
+ * @author mwittig
+ * 
+ */
+public final class KXSyncCommandFunction extends AKXSyncCommand {
+	
+	/** Function. */
+	private final Function function;
+	
+	
+	/**
+	 * @param aFunction Function
+	 */
+	public KXSyncCommandFunction(final Function aFunction) {
+		super();
+		this.function = aFunction;
+	}
+	
+	/**
+	 * @return the select
+	 */
+	public Function getFunction() {
+		return this.function;
+	}
+	
+	@Override
+	public Result execute(final c c) throws KXException, KException, IOException {
+		return this.execute(c, this.function.toQ());
+	}
+	
+}
